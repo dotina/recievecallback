be853ee42809ba1cead6db01a23ef120e74e73f6
dotina <derrick.angolo@live.com>
2020-08-17 21:14:52 +0300

    updating service port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3300;
 
 app.use(cors());
 // Configuring body parser middleware
