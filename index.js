081faee7d9e4ce3d679968ca2056d1b2d2191c8e
dotina <derrick.angolo@live.com>
2020-08-17 14:01:12 +0300

    the first push

---
+++ index.js
@@ -0,0 +1,55 @@
+const {get,post} = require('./netUtils');
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/recievecallback', async (req, res) => {
+    // We will be coding here
+    const body='body' in req ? req.body : null;
+    const headers='headers' in req ? req.headers : null;
+    const keyEndpoint = 'key-endpoint' in headers ? headers['key-endpoint'] : null;
+
+    if (!body) {
+    	res.send(400, 'include a valid body');
+    }else if(!headers){
+    	res.send(400, 'include a valid headers');
+    }else if(!keyEndpoint){
+    	res.send(400, 'missing destination endpoint in headers | check DB');
+    }else{
+    	post(keyEndpoint,body,{},null,null)
+    	.then(response=>{
+    		console.log('Incoming response : ', response.body);
+    		if (response.statusCode != 200) {
+    			res.send(response.statusCode,{
+	    			responsecode:response.statusCode,
+	    			message: response.body,
+	    			status:false
+	    		})
+    		}else{
+	    		res.send(response.statusCode,{
+	    			responsecode:response.statusCode,
+	    			message: response.body,
+	    			status:true
+	    		})
+    		}
+    	})
+    	.catch(err=>{
+    		console.log('Error : ', err);
+    		res.send(500,{
+    			responsecode:500,
+    			message:'service error',
+    			err,
+    			status:false
+    		})
+    	})
+    }
+
+  
+});
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
---
+++ netUtils.js
@@ -0,0 +1,33 @@
+const axios = require('axios').default
+const get = (endpointURL, bearer_token = null, basic_token = null) => requestWithAxios('GET', endpointURL, null, {}, bearer_token, basic_token)
+const post = (endpointURL, params, headers = {}, bearer_token = null, basic_token = null) => requestWithAxios('POST', endpointURL, params, headers, bearer_token, basic_token)
+const requestWithAxios = (method, url, params, headers, bearer_token, basic_token) => new Promise((resolve, reject) => {
+    const options = {
+        method,
+        url,
+        headers: {
+            Accept: 'application/json',
+            'Accept-Encoding': 'application/json',
+            'Accept-Language': 'EN',
+            'Content-Type': 'application/json'
+        },
+    }
+    if (params) options.data = params
+    if (headers) options.headers = {...options.headers,...headers}
+    if (bearer_token) options.headers.Authorization = `Bearer ${bearer_token}`
+    if (basic_token) options.headers.Authorization = `Basic ${basic_token}`
+    // console.log('RWA_FUNC => Outgoing Options: ',options)
+    axios(options)
+    .then((response) => resolve({ statusCode: response.status, body: response.data }) )
+    .catch((error) => {
+        if (error.response !== undefined) {
+            resolve({ statusCode: error.response.status, body: error.response.data, error })
+        } else if (error.isAxiosError) {
+            resolve({ statusCode: 999, body: { message: error.request.response || 'HTTP Client Error', error } })
+        } else {
+        // eslint-disable-next-line prefer-promise-reject-errors
+            reject({ statusCode: 999, body: { message: 'message' in error ? error.message : 'Error did not have a message', error } })
+        }
+    })
+})
+module.exports={get,post}
