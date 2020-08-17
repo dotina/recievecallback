6dc2d3632d31c215bf239ce2ec66f2633c254f27
dotina <derrick.angolo@live.com>
2020-08-17 15:40:38 +0300

    update on logging and response types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const {get,post} = require('./netUtils');
 const express = require('express')
 const bodyParser = require('body-parser');
@@ -16,22 +17,28 @@ app.post('/recievecallback', async (req, res) => {
     const keyEndpoint = 'key-endpoint' in headers ? headers['key-endpoint'] : null;
 
     if (!body) {
-    	res.send(400, 'include a valid body');
+    	fs.appendFileSync("./logs/error.log",JSON.stringify({message:'include a valid body',timestamp:new Date().toLocaleString()}));
+    	res.status(400).json({responseCode:400,status:false,message: 'include a valid body'});
     }else if(!headers){
-    	res.send(400, 'include a valid headers');
+    	fs.appendFileSync("./logs/error.log",JSON.stringify({message:'include a valid headers',timestamp:new Date().toLocaleString()}));
+    	res.status(400).json({responseCode:400,status:false,message: 'include a valid headers'});
     }else if(!keyEndpoint){
-    	res.send(400, 'missing destination endpoint in headers | check DB');
+    	fs.appendFileSync("./logs/error.log",JSON.stringify({message:'missing destination endpoint in headers | check DB',timestamp:new Date().toLocaleString()}));
+    	res.status(400).json({responseCode:400,status:false,message: 'missing destination endpoint in headers | check DB'});
     }else{
     	post(keyEndpoint,body,{},null,null)
     	.then(response=>{
     		console.log('Incoming response : ', response.body);
+    		
     		if (response.statusCode != 200) {
+    			fs.appendFileSync("./logs/error.log",JSON.stringify({...response.body,timestamp:new Date().toLocaleString()}));
     			res.status(response.statusCode).json({
 	    			responsecode:response.statusCode,
 	    			message: response.body,
 	    			status:false
 	    		})
     		}else{
+    			fs.appendFileSync("./logs/response.log",JSON.stringify({...response.body,timestamp:new Date().toLocaleString()}));
 	    		res.status(response.statusCode).json({
 	    			responsecode:response.statusCode,
 	    			message: response.body,
@@ -41,6 +48,7 @@ app.post('/recievecallback', async (req, res) => {
     	})
     	.catch(err=>{
     		console.log('Error : ', err);
+    		fs.appendFileSync("./logs/error.log",JSON.stringify({err,timestamp:new Date().toLocaleString()}));
     		res.status(500).json({
     			responsecode:500,
     			message:'service error',
